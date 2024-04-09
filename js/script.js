4fcae4c0bf9abb3c0b3cd69d04368e5f2728ec2d
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-04-09 18:09:56 +0200

    fix: conditions

--- js/script.js
+++ js/script.js
@@ -16,7 +16,7 @@ createApp({
     methods: {
         generateTenRandomMails(){
             
-            for(let i = 0; i <= 10; i++){
+            for(let i = 0; i < 10; i++){
                 axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`)
                 .then((response) => {
                 let rndmMail = response.data.response;
